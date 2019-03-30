60a9355561a295b445e0d44bf34c83c55b58d416
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-03-30 18:17:47 +0300

    add history cell

---
+++ Guess the Place!/Sources/Views/Cells/History/HistoryTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryTableViewCell.swift
+//  Guess the Place!
+//
+//  Created by Nikita Teslyuk on 30/03/2019.
+//  Copyright © 2019 Tesnik. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+}
